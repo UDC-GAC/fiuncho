740b314908fcd078c04faee487555d88d8079046
Christian Ponte <christian.ponte@udc.es>
2021-09-09 19:52:52 +0200

    Fix mi calculation

--- src/cpu/base/MutualInformation.cpp
+++ src/cpu/base/MutualInformation.cpp
@@ -46,20 +46,25 @@ template <>
 float MutualInformation<float>::compute<uint32_t>(
     const ContingencyTable<uint32_t> &table) const noexcept
 {
-    float p_case, p_ctrl, p_any, log_p_case, log_p_ctrl, log_p_any, h_x = 0.0,
-                                                                    h_all = 0.0;
+    float p_case, p_ctrl, p_any, h_x = 0.0f, h_all = 0.0f;
     // Constant references to objects are not enough for the compiler to
     // determine the number of loop iterations for vectorization, therefore they
     // have to be copied to a local constant. GCC also needs the "-ffast-math"
     // optimization parameter to vectorize this loop.
-    const size_t iters = table.size;
-    for (size_t i = 0; i < iters; i++) {
+    const size_t size = table.size;
+    for (size_t i = 0; i < size; i++) {
         p_case = table.cases[i] * inv_inds;
-        h_all -= p_case * logf(p_case);
+        if (table.cases[i] != 0.0f){
+            h_all -= p_case * logf(p_case);
+        }
         p_ctrl = table.ctrls[i] * inv_inds;
-        h_all -= p_ctrl * logf(p_ctrl);
+        if (table.ctrls[i] != 0.0f){
+            h_all -= p_ctrl * logf(p_ctrl);
+        }
         p_any = p_case + p_ctrl;
-        h_x -= p_any * logf(p_any);
+        if (p_any != 0.0f){
+            h_x -= p_any * logf(p_any);
+        }
     }
 
     return h_x + h_y - h_all;
