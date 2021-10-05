a738ac3daa2fed4bdabb8d5c145a29540387300f
Christian Ponte <christian.ponte@udc.es>
2021-10-05 15:49:05 +0200

    Simplify code

--- src/cpu/avx512f512/GenotypeTable.cpp
+++ src/cpu/avx512f512/GenotypeTable.cpp
@@ -14,72 +14,45 @@ GenotypeTable<uint64_t>::GenotypeTable(const short order,
 {
 }
 
+inline void combine_aux(const uint64_t *gt_tbl1, const size_t gt_size,
+                        const uint64_t *gt_tbl2, const size_t words,
+                        uint64_t *gt_tbl3)
+{
+    size_t i, j, k;
+    // Combine two genotype tables and store it in a larger table
+    // Outer loop is unrolled by a factor of 3
+    for (i = 0; i < gt_size; i += 3) {
+        for (j = 0; j < 3; j++) {
+            for (k = 0; k < words; k += 8) {
+                __m512i z0 =
+                    _mm512_load_si512((__m512i *)(gt_tbl2 + j * words + k));
+                __m512i z1 = _mm512_load_si512(
+                    (__m512i *)(gt_tbl1 + (i + 0) * words + k));
+                __m512i z2 = _mm512_load_si512(
+                    (__m512i *)(gt_tbl1 + (i + 1) * words + k));
+                __m512i z3 = _mm512_load_si512(
+                    (__m512i *)(gt_tbl1 + (i + 2) * words + k));
+                __m512i z4 = _mm512_and_si512(z0, z1);
+                __m512i z5 = _mm512_and_si512(z0, z2);
+                __m512i z6 = _mm512_and_si512(z0, z3);
+                _mm512_store_si512(
+                    (__m512i *)(gt_tbl3 + ((i + j) * 3 + 0) * words + k), z4);
+                _mm512_store_si512(
+                    (__m512i *)(gt_tbl3 + ((i + j) * 3 + 1) * words + k), z5);
+                _mm512_store_si512(
+                    (__m512i *)(gt_tbl3 + ((i + j) * 3 + 2) * words + k), z6);
+            }
+        }
+    }
+}
+
 template <>
 void GenotypeTable<uint64_t>::combine(const GenotypeTable<uint64_t> &t1,
                                       const GenotypeTable<uint64_t> &t2,
                                       GenotypeTable<uint64_t> &out) noexcept
 {
-    size_t i, j, k;
-    // Compute bit tables for cases
-    for (i = 0; i < t1.size; i += 3) {
-        for (j = 0; j < 3; j++) {
-            for (k = 0; k < t1.cases_words; k += 8) {
-                __m512i z0 = _mm512_load_si512(
-                    (__m512i *)(t2.cases + j * t1.cases_words + k));
-                __m512i z1 = _mm512_load_si512(
-                    (__m512i *)(t1.cases + (i + 0) * t1.cases_words + k));
-                __m512i z2 = _mm512_load_si512(
-                    (__m512i *)(t1.cases + (i + 1) * t1.cases_words + k));
-                __m512i z3 = _mm512_load_si512(
-                    (__m512i *)(t1.cases + (i + 2) * t1.cases_words + k));
-                __m512i z4 = _mm512_and_si512(z0, z1);
-                __m512i z5 = _mm512_and_si512(z0, z2);
-                __m512i z6 = _mm512_and_si512(z0, z3);
-                _mm512_store_si512(
-                    (__m512i *)(out.cases + ((i + j) * 3 + 0) * t1.cases_words +
-                                k),
-                    z4);
-                _mm512_store_si512(
-                    (__m512i *)(out.cases + ((i + j) * 3 + 1) * t1.cases_words +
-                                k),
-                    z5);
-                _mm512_store_si512(
-                    (__m512i *)(out.cases + ((i + j) * 3 + 2) * t1.cases_words +
-                                k),
-                    z6);
-            }
-        }
-    }
-    // Compute bit tables for ctrls
-    for (i = 0; i < t1.size; i += 3) {
-        for (j = 0; j < 3; j++) {
-            for (k = 0; k < t1.ctrls_words; k += 8) {
-                __m512i z0 = _mm512_load_si512(
-                    (__m512i *)(t2.ctrls + j * t1.ctrls_words + k));
-                __m512i z1 = _mm512_load_si512(
-                    (__m512i *)(t1.ctrls + (i + 0) * t1.ctrls_words + k));
-                __m512i z2 = _mm512_load_si512(
-                    (__m512i *)(t1.ctrls + (i + 1) * t1.ctrls_words + k));
-                __m512i z3 = _mm512_load_si512(
-                    (__m512i *)(t1.ctrls + (i + 2) * t1.ctrls_words + k));
-                __m512i z4 = _mm512_and_si512(z0, z1);
-                __m512i z5 = _mm512_and_si512(z0, z2);
-                __m512i z6 = _mm512_and_si512(z0, z3);
-                _mm512_store_si512(
-                    (__m512i *)(out.ctrls + ((i + j) * 3 + 0) * t1.ctrls_words +
-                                k),
-                    z4);
-                _mm512_store_si512(
-                    (__m512i *)(out.ctrls + ((i + j) * 3 + 1) * t1.ctrls_words +
-                                k),
-                    z5);
-                _mm512_store_si512(
-                    (__m512i *)(out.ctrls + ((i + j) * 3 + 2) * t1.ctrls_words +
-                                k),
-                    z6);
-            }
-        }
-    }
+    combine_aux(t1.cases, t1.size, t2.cases, t1.cases_words, out.cases);
+    combine_aux(t1.ctrls, t1.size, t2.ctrls, t1.ctrls_words, out.ctrls);
 }
 
 const __m512i lookup = _mm512_setr_epi64(
@@ -93,7 +66,7 @@ inline void popcnt_aux(const uint64_t *gt_tbl1, const size_t gt_size,
                        uint32_t *ct_tbl, const size_t ct_size)
 {
     size_t i, j, k, l;
-    // Compute count tables for cases
+    // Combine two genotype tables and save its contingency table
     for (i = 0; i < gt_size; ++i) {
         for (j = 0; j < 3; ++j) {
             k = 0;
