558fdc5fea99d1452649f895cc222493cbe68bcc
Christian Ponte <christian.ponte@udc.es>
2021-10-05 15:14:20 +0200

    Improve code

--- src/cpu/avx512f512/GenotypeTable.cpp
+++ src/cpu/avx512f512/GenotypeTable.cpp
@@ -82,76 +82,71 @@ void GenotypeTable<uint64_t>::combine(const GenotypeTable<uint64_t> &t1,
     }
 }
 
+const __m512i lookup = _mm512_setr_epi64(
+    0x0302020102010100llu, 0x0403030203020201llu, 0x0302020102010100llu,
+    0x0403030203020201llu, 0x0302020102010100llu, 0x0403030203020201llu,
+    0x0302020102010100llu, 0x0403030203020201llu);
+const __m512i low_mask = _mm512_set1_epi8(0x0f);
+
+inline void popcnt_aux(const uint64_t *gt_tbl1, const size_t gt_size,
+                       const uint64_t *gt_tbl2, const size_t words,
+                       uint32_t *ct_tbl, const size_t ct_size)
+{
+    size_t i, j, k, l;
+    // Compute count tables for cases
+    for (i = 0; i < gt_size; ++i) {
+        for (j = 0; j < 3; ++j) {
+            k = 0;
+            __m512i acc = _mm512_setzero_si512();
+            while (k < words) {
+                // 512-bit value holding the number of individuals counted in
+                // each 8-bit value inside the 512-bit word
+                __m512i local = _mm512_setzero_si512();
+                // Each 8-bit integer can represent a value in [0, 255], and
+                // each iteration can increase the count by 8, so the maximum
+                // number of iterations before clearing the buffer is 255/8
+                for (l = 0; l < 255 / 8 && k < words; ++l, k += 8) {
+                    // Read values to combine from input genotype tables
+                    __m512i z0 =
+                        _mm512_load_si512((__m512i *)(gt_tbl2 + j * words + k));
+                    __m512i z1 =
+                        _mm512_load_si512((__m512i *)(gt_tbl1 + i * words + k));
+                    // Find the intersection of individuals
+                    __m512i z2 = _mm512_and_si512(z0, z1);
+                    // Count the number of individuals on the intersection
+                    const __m512i lo = _mm512_and_si512(z2, low_mask);
+                    const __m512i hi =
+                        _mm512_and_si512(_mm512_srli_epi32(z2, 4), low_mask);
+                    const __m512i popcnt1 = _mm512_shuffle_epi8(lookup, lo);
+                    const __m512i popcnt2 = _mm512_shuffle_epi8(lookup, hi);
+                    local = _mm512_add_epi8(local, popcnt1);
+                    local = _mm512_add_epi8(local, popcnt2);
+                }
+                // Once the 512-bit buffer holding the 8-bit integers cannot
+                // safely count more individuals, or there are no more words to
+                // process, accumulate the results in a 512-bit buffer
+                // containing 64-bit integers
+                acc = _mm512_add_epi64(
+                    acc, _mm512_sad_epu8(local, _mm512_setzero_si512()));
+            }
+            // Once all the words of a row have been processed, reduce the sum
+            // into a single 64-bit value and store it in the contingency table
+            ct_tbl[i * 3 + j] = _mm512_reduce_add_epi64(acc);
+        }
+    }
+    for (i = gt_size * 3; i < ct_size; ++i) {
+        ct_tbl[i] = 0;
+    }
+}
+
 template <>
 template <>
 void GenotypeTable<uint64_t>::combine_and_popcnt(
     const GenotypeTable<uint64_t> &t1, const GenotypeTable<uint64_t> &t2,
     ContingencyTable<uint32_t> &out) noexcept
 {
-    const __m512i lookup = _mm512_setr_epi64(
-        0x0302020102010100llu, 0x0403030203020201llu, 0x0302020102010100llu,
-        0x0403030203020201llu, 0x0302020102010100llu, 0x0403030203020201llu,
-        0x0302020102010100llu, 0x0403030203020201llu);
-    const __m512i low_mask = _mm512_set1_epi8(0x0f);
-    size_t i, j, k, l;
-    // Compute count tables for cases
-    for (i = 0; i < t1.size; ++i) {
-        for (j = 0; j < 3; ++j) {
-            k = 0;
-            __m512i acc = _mm512_setzero_si512();
-            while (k < t1.cases_words) {
-                __m512i local = _mm512_setzero_si512();
-                for (l = 0; l < 255 / 8 && k < t1.cases_words; ++l, k += 8) {
-                    __m512i z0 = _mm512_load_si512(
-                        (__m512i *)(t2.cases + j * t1.cases_words + k));
-                    __m512i z1 = _mm512_load_si512(
-                        (__m512i *)(t1.cases + i * t1.cases_words + k));
-                    __m512i z4 = _mm512_and_si512(z0, z1);
-                    const __m512i lo = _mm512_and_si512(z4, low_mask);
-                    const __m512i hi =
-                        _mm512_and_si512(_mm512_srli_epi32(z4, 4), low_mask);
-                    const __m512i popcnt1 = _mm512_shuffle_epi8(lookup, lo);
-                    const __m512i popcnt2 = _mm512_shuffle_epi8(lookup, hi);
-                    local = _mm512_add_epi8(local, popcnt1);
-                    local = _mm512_add_epi8(local, popcnt2);
-                }
-                acc = _mm512_add_epi64(
-                    acc, _mm512_sad_epu8(local, _mm512_setzero_si512()));
-            }
-            out.cases[i * 3 + j] = _mm512_reduce_add_epi64(acc);
-        }
-    }
-    for (i = t1.size * 3; i < out.size; ++i) {
-        out.cases[i] = 0;
-    }
-    // Compute count tables for ctrls
-    for (i = 0; i < t1.size; ++i) {
-        for (j = 0; j < 3; ++j) {
-            k = 0;
-            __m512i acc = _mm512_setzero_si512();
-            while (k < t1.ctrls_words) {
-                __m512i local = _mm512_setzero_si512();
-                for (l = 0; l < 255 / 8 && k < t1.ctrls_words; ++l, k += 8) {
-                    __m512i z0 = _mm512_load_si512(
-                        (__m512i *)(t2.ctrls + j * t1.ctrls_words + k));
-                    __m512i z1 = _mm512_load_si512(
-                        (__m512i *)(t1.ctrls + i * t1.ctrls_words + k));
-                    __m512i z4 = _mm512_and_si512(z0, z1);
-                    const __m512i lo = _mm512_and_si512(z4, low_mask);
-                    const __m512i hi =
-                        _mm512_and_si512(_mm512_srli_epi32(z4, 4), low_mask);
-                    const __m512i popcnt1 = _mm512_shuffle_epi8(lookup, lo);
-                    const __m512i popcnt2 = _mm512_shuffle_epi8(lookup, hi);
-                    local = _mm512_add_epi8(local, popcnt1);
-                    local = _mm512_add_epi8(local, popcnt2);
-                }
-                acc = _mm512_add_epi64(
-                    acc, _mm512_sad_epu8(local, _mm512_setzero_si512()));
-            }
-            out.ctrls[i * 3 + j] = _mm512_reduce_add_epi64(acc);
-        }
-    }
-    for (i = t1.size * 3; i < out.size; ++i) {
-        out.ctrls[i] = 0;
-    }
+    popcnt_aux(t1.cases, t1.size, t2.cases, t1.cases_words, out.cases,
+               out.size);
+    popcnt_aux(t1.ctrls, t1.size, t2.ctrls, t2.ctrls_words, out.ctrls,
+               out.size);
 }
