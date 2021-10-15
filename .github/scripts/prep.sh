d89f036569c1d59bc7f366d8345d6467532bb291
Christian Ponte <christian.ponte@udc.es>
2021-10-15 15:23:27 +0200

    Change user agent during Intel SDE download

--- .github/scripts/prep.sh
+++ .github/scripts/prep.sh
@@ -14,7 +14,8 @@ sudo apt-get install ${COMPILER_PACKAGES} ${MPI_PACKAGES} -qq 1>/dev/null
 INTEL_SDE_URL="https://software.intel.com/content/dam/develop/external/\
 us/en/documents/downloads/sde-external-8.69.1-2021-07-18-lin.tar.bz2"
 INTEL_SDE_TARFILE=$(echo ${INTEL_SDE_URL} | rev | cut -d '/' -f 1 | rev)
-wget -q ${INTEL_SDE_URL}
+wget -q --user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 \
+(KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" ${INTEL_SDE_URL}
 mkdir intel_sde/
 tar -xf ${INTEL_SDE_TARFILE} --strip-components=1 -C intel_sde/
 echo "$(pwd)/intel_sde" >> $GITHUB_PATH
