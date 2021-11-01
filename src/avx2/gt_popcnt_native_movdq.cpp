940da0b0caea24319f88408804c3f35e08042fb2
Christian Ponte <christian.ponte@udc.es>
2021-11-01 08:26:52 +0100

    Fix AVX2 popcnt movdq implementation including a AVX512DQ instruction

--- src/avx2/gt_popcnt_native_movdq.cpp
+++ src/avx2/gt_popcnt_native_movdq.cpp
@@ -56,13 +56,13 @@ inline void avx2_popcnt_64_native_movdq(const uint64_t *gt_tbl1,
                 __m256i w2 =
                     _mm256_load_si256((__m256i *)(gt_tbl2 + j * words + k));
                 __m256i inters = _mm256_and_si256(w1, w2);
-                const __m128i lo = _mm256_extracti64x2_epi64(inters, 0);
+                const __m128i lo = _mm256_extracti128_si256(inters, 0);
                 uint64_t lower64 = _mm_cvtsi128_si64(lo);
                 cnt += _mm_popcnt_u64(lower64);
                 __m128i temp2 = (__m128i)_mm_movehl_ps((__m128)lo, (__m128)lo);
                 uint64_t upper64 = _mm_cvtsi128_si64(temp2);
                 cnt += _mm_popcnt_u64(upper64);
-                const __m128i hi = _mm256_extracti64x2_epi64(inters, 1);
+                const __m128i hi = _mm256_extracti128_si256(inters, 1);
                 lower64 = _mm_cvtsi128_si64(hi);
                 cnt += _mm_popcnt_u64(lower64);
                 temp2 = (__m128i)_mm_movehl_ps((__m128)hi, (__m128)hi);
